7e4cf70cd49f6661c0bd7b6820f7b6e9fbca83b4
me-cafebabe <me_cafebabe@hotmail.com>
2026-05-14 22:40:46 +0900

    X3-KC_sprout: Setup Retrofit Dynamic Partitions

--- device.mk
+++ device.mk
@@ -51,6 +51,11 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
+# Dynamic Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
