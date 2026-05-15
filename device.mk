f004e396bbde483653251261e0ebdc1866f45e1e
kailua <kailua@project2by2.jp>
2026-05-15 18:24:13 +0900

    X3-KC_sprout: Drop livedisplay for now

--- device.mk
+++ device.mk
@@ -246,10 +246,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay-service.sdm
-
 # Mac address setup
 PRODUCT_PACKAGES += \
     macaddrsetup
